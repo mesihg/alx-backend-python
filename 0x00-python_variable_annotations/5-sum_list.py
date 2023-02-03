80eda83afc9284f1493124d5d20855ce40714b86
Mesih Grmay <mesihgrmay@yahoo.com>
2023-02-03 02:34:12 +0000

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Task 5 to string module"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum of a list of floating numbers"""
+    return float(sum(input_list))
