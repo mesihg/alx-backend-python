9f338f52bf9beae59104715903c0026f205da080
Mesih Grmay <mesihgrmay@yahoo.com>
2023-02-03 02:18:01 +0000

    Task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 """Task 1 concatenation module"""
 
 
-def add(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """Concatenate two strings"""
     return "{}{}".format(str1, str2)
