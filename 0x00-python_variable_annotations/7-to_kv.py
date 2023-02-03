0b5482bf8bf02336168fac33698829acaf9686a0
Mesih Grmay <mesihgrmay@yahoo.com>
2023-02-03 02:54:49 +0000

    Task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,5 +4,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """returns tuple of the given input"""
-    return (k, float(v**2)
+    """returns a tuple"""
+    return (k, float(v**2))
