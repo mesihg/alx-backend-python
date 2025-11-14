e4f0c3809447fcf85713292688ad86f4b6ae5d8a
mesihg <mesihgrmay@yahoo.com>
2025-11-14 17:56:48 +0300

    Defining data models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,90 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Custom User model which extends AbstractUser"""
+
+    user_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        db_index=True
+    )
+    email = models.EmailField(
+        unique=True,
+        db_index=True,
+        null=False,
+        blank=False
+    )
+
+    phone_number = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+    )
+
+    class Role(models.TextChoices):
+        GUEST = 'guest', 'Guest'
+        HOST = 'host', 'Host'
+        ADMIN = 'admin', 'Admin'
+
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.GUEST
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
+
+
+class Conversation(models.Model):
+    """Tracks a conversation and all its participants"""
+
+    conversation_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        db_index=True
+    )
+    participants = models.ManyToManyField(
+        User,
+        related_name='conversations'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.conversation_id)
+
+
+class Message(models.Model):
+    """Represents a single message sent by a user"""
+
+    message_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        db_index=True
+    )
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="sent_messages"
+    )
+    conversation = models.ForeignKey(
+        Conversation,
+        on_delete=models.CASCADE,
+        related_name="messages"
+    )
+    message_body = models.TextField(null=False, blank=False)
+
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender} at {str(self.sent_at)}"
