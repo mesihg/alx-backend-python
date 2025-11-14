74518eeab2669dac513cba48efb2180ebade0bcf
mesihg <mesihgrmay@yahoo.com>
2025-11-14 21:07:47 +0300

    Defining serializers

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import User, Conversation, Message
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "user_id",
+            "email",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "role",
+            "created_at",
+        ]
+        read_only_fields = ["user_id", "created_at"]
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = [
+            "message_id",
+            "sender",
+            "message_body",
+            "sent_at",
+            "conversation",
+        ]
+        read_only_fields = ["message_id", "sent_at", "sender"]
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Conversation
+        fields = [
+            "conversation_id",
+            "participants",
+            "messages",
+            "created_at",
+        ]
+        read_only_fields = ["conversation_id", "created_at"]
