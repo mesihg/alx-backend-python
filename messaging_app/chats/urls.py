ea91911ea93ec52486e03f5738f7911ef01e3e97
mesihg <mesihgrmay@yahoo.com>
2025-11-15 17:36:25 +0300

    Modifying chat urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,8 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from django.urls import path, include
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r"conversations", views.ConversationViewSet,
                 basename="conversations")
 router.register(r"messages", views.MessageViewSet, basename="messages")
