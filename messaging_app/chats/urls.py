e7f3cf38cc8493775e2c56619f935a8990925336
mesihg <mesihgrmay@yahoo.com>
2025-11-15 17:50:04 +0300

    Modifying chat urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,13 +1,21 @@
 from rest_framework import routers
+from rest_framework_nested.routers import NestedDefaultRouter
 from django.urls import path, include
 from . import views
 
 router = routers.DefaultRouter()
 router.register(r"conversations", views.ConversationViewSet,
                 basename="conversations")
-router.register(r"messages", views.MessageViewSet, basename="messages")
+
+conversations_router = NestedDefaultRouter(
+    router, r"conversations", lookup="conversation")
+
+# router.register(r"messages", views.MessageViewSet, basename="messages")
+conversations_router.register(
+    r"messages", views.MessageViewSet, basename="conversation-messages")
 
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("", include(conversations_router.urls)),
 ]
