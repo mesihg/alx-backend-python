837b4695d21a36639016590779e9b0463bedd68e
mesihg <mesihgrmay@yahoo.com>
2025-11-23 22:29:18 +0300

    Implementing authentication and permission

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -46,6 +46,12 @@ INSTALLED_APPS = [
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
