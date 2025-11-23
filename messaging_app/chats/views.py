d604f7a46842f248e9da3caac913516ae2c112e4
mesihg <mesihgrmay@yahoo.com>
2025-11-23 22:51:05 +0300

    Implementing authentication and permission

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,16 +1,38 @@
 from rest_framework import viewsets, status, filters, permissions
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound, PermissionDenied
 from .models import Message, Conversation
 from .serializers import MessageSerializer, ConversationSerializer
-from .permissions import IsParticipantOfConversation
+from .permissions import IsParticipantOfConversation, IsMessageOwnerOrReadOnly
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [
+        permissions.IsAuthenticated,
+        IsParticipantOfConversation,
+        IsMessageOwnerOrReadOnly
+    ]
     queryset = Message.objects.all()
 
     filter_backends = [filters.OrderingFilter]
+
+    def get_queryset(self):
+        conversation_id = self.kwargs.get("conversation_id")
+        try:
+            conversation = Conversation.objects.get(
+                conversation_id=conversation_id)
+        except Conversation.DoesNotExist:
+            raise NotFound("Conversation not found")
+        if not conversation.participants.filter(pk=self.request.user.pk).exists():
+            raise PermissionDenied(
+                "You are not a participant in this conversation",
+                code=status.HTTP_403_FORBIDDEN,
+            )
+        queryset = Message.objects.filter(conversation=conversation)
+        queryset = self.filter_queryset(queryset)
+
+        return queryset.select_related("sender", "conversation").order_by("-sent_at")
 
     def list(self, request, *args, **kwargs):
         messages = self.get_queryset()
